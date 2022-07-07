c613e61491087b42af018b00686dcd5d815bbe30
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-06 21:34:42 -0300

    Inserir atualizações feitas na classe

--- src/contacorrente/ContaCorrente.java
+++ src/contacorrente/ContaCorrente.java
@@ -1,5 +1,18 @@
 package contacorrente;
 
 public class ContaCorrente {
+    int numeroDaConta;
+    String nomeDoCorrentista;
+    double saldo;
 
+    public ContaCorrente() {
+        this.numeroDaConta = 6432;
+        this.nomeDoCorrentista = "Kevin Richard Da Silva Candido";
+        this.saldo = 0;
+
+    }
+
+    public void status() {
+        System.out.println("Número da conta" + this.numeroDaConta + "\npertence ao correntista " + this.nomeDoCorrentista + "\ncom um total de saldo de " + this.saldo);
+    }
 }
--- src/contacorrente/Main.java
+++ src/contacorrente/Main.java
@@ -2,6 +2,10 @@ package contacorrente;
 
 public class Main {
     public static void main(String[] args) {
-
+        ContaCorrente contaBancaria = new ContaCorrente();
+        contaBancaria.numeroDaConta = 1765;
+        contaBancaria.nomeDoCorrentista = "Kevin Richard";
+        contaBancaria.saldo = 0;
+        contaBancaria.status();
     }
 }
