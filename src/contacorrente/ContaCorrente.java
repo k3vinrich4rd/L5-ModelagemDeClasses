431e40096956479152f363f442d1458bf3595f1e
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-08 17:34:25 -0300

    Inserindo atualizações na classe corrente

--- src/contacorrente/ContaCorrente.java
+++ src/contacorrente/ContaCorrente.java
@@ -5,14 +5,28 @@ public class ContaCorrente {
     String nomeDoCorrentista;
     double saldo;
 
+    double saque;
+    double deposito;
+
     public ContaCorrente() {
         this.numeroDaConta = 6432;
         this.nomeDoCorrentista = "Kevin Richard Da Silva Candido";
         this.saldo = 0;
+        this.saque = 0;
+        this.deposito = 0;
+
+    }
+
+    public double depositoConta() {
+        return saldo += deposito;
+    }
+
+    public double saqueConta() {
+        return saldo -= saque;
 
     }
 
     public void status() {
-        System.out.println("Número da conta: " + this.numeroDaConta + "\npertence ao correntista: " + this.nomeDoCorrentista + "\ncom um total de saldo de " + this.saldo);
+        System.out.println("Número da conta: " + this.numeroDaConta + "\npertence ao correntista: " + this.nomeDoCorrentista);
     }
 }
--- src/contacorrente/MainContaCorrente.java
+++ src/contacorrente/MainContaCorrente.java
@@ -5,7 +5,16 @@ public class MainContaCorrente {
         ContaCorrente contaBancaria = new ContaCorrente();
         contaBancaria.numeroDaConta = 1765;
         contaBancaria.nomeDoCorrentista = "Kevin Richard";
-        contaBancaria.saldo = 50;
+        contaBancaria.saldo = 1500;
+        contaBancaria.deposito = 500;
+        contaBancaria.saque = 200;
         contaBancaria.status();
+
+        System.out.println("O seu saldo era de: " + contaBancaria.saldo + "R$" + "\n" +
+                "Você fez um deposito na sua conta, agora seu saldo é: " + contaBancaria.depositoConta()
+                + "R$ " + "\nVocê fez um saque, e agora seu saldo total é de: " + contaBancaria.saqueConta() + "R$");
+
+
     }
+
 }
